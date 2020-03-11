a0c17d51f85d715ed162553553b55421a8db805b
linur <raphaelroy@tutanota.com>
2020-03-10 23:17:48 -0400

    clean load of CIFAR10 dataset

--- cnn_active_learning/cnn_active_learning.py
+++ cnn_active_learning/cnn_active_learning.py
@@ -4,9 +4,14 @@ import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
 
-CIFAR10.download()
-data,labels,filenames = CIFAR10.unpickle('./data/cifar-10-batches-py/data_batch_3')
-# print(data.shape, len(labels), len(filenames))
-print(labels[0], filenames[0])
+#CIFAR10.download()
+dataset = CIFAR10()
+
+data,labels,label_names = dataset.get_data()
+
+
+
+print(data.shape, len(labels), len(label_names))
+#print(labels[0], filenames[0])
 
 
--- cnn_active_learning/dataset.py
+++ cnn_active_learning/dataset.py
@@ -3,6 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 import pickle
 import subprocess
+import numpy as np
 
 # class model
 class MyDataset(object):
@@ -12,22 +13,46 @@ class MyDataset(object):
 
     def download(self):
         pass
-
+    
+    def get_data(self):
+        pass
 
 class CIFAR10(MyDataset):
 
     @staticmethod
     def unpickle(file):
         with open(file, 'rb') as fo:
-            dico = pickle.load(fo, encoding='latin1')
-        data = dico.get('data')
-        labels = dico.get('labels') #narray
-        filenames = dico.get('filenames')
-        return data,labels,filenames
+            dic = pickle.load(fo, encoding='latin1')
+        return dic
 
     # Call a script to download CIFAR10 dataset
     @staticmethod
     def download():
         subprocess.call(["./dl-CIFAR10.sh"])
     
+    @staticmethod 
+    def get_data():
+
+        data_dir = "./data/cifar-10-batches-py/"
+        filenames = ["data_batch_1","data_batch_2","data_batch_3","data_batch_4","data_batch_5"]
+        data = None
+        labels = None
+        for filename in filenames:
+            filepath = data_dir + filename
+            data_dic = CIFAR10.unpickle(filepath)
+            if data is None:
+                #data = np.empty(data_dic['data'].shape, len(filenames))
+                data = data_dic['data']
+                labels = data_dic['labels']
+                print(len(data))
+                print(len(labels))
+            else:
+
+                data = np.append(data, data_dic['data'], axis=0)
+                labels = np.append(labels, data_dic['labels'])
+                print(len(data))
+
+        label_dic = CIFAR10.unpickle('./data/cifar-10-batches-py/batches.meta')
+        label_names = label_dic['label_names']
+        return data, labels, label_names
 
