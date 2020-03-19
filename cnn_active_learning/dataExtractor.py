756365a3b28bb1f836bb24a446906844cb7ea70a
Clement <gicquel.clem@gmail.com>
2020-03-19 13:28:42 -0400

    CIFAR100

--- cnn_active_learning/dataExtractor.py
+++ cnn_active_learning/dataExtractor.py
@@ -77,3 +77,55 @@ class CIFAR10Extractor(DataExtractor):
         label_names = label_dic['label_names']
         return label_names
 
+
+
+
+class CIFAR100Extractor(DataExtractor):
+    data_dir = './data/cifar-100-batches-py/'
+
+    def download(self):
+        subprocess.call(['./dl-CIFAR100.sh'])
+
+    def unpickle(self, file):
+        with open(file, 'rb') as fo:
+            dic = pickle.load(fo, encoding='latin1')
+        return dic
+
+    def extract_pool_data(self):
+        filenames = ['data_batch_1']  # reduce data size for dev (faster)
+        # filenames = ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5']
+        init = False
+        for filename in filenames:
+            filepath = self.data_dir + filename
+            pool_dic = self.unpickle(filepath)
+            data = pool_dic['data']
+            labels = pool_dic['labels']
+            if not init:
+                init = True
+                pool_samples = np.array(data)
+                pool_labels = np.array(labels)
+            else:
+                pool_samples = np.append(pool_samples, data, axis=0)
+                pool_labels = np.append(pool_labels, labels)
+
+        pool_samples = np.vstack(pool_samples).reshape(-1, 3, 32, 32)
+        return pool_samples, pool_labels
+
+    @staticmethod
+    def show_image(images, index):
+        images = images.transpose((0, 2, 3, 1))
+        img = images[index]
+        plt.imshow(img)
+        plt.show()
+
+    def extract_test_data(self):
+        filepath = self.data_dir + 'test_batch'
+        test_dic = self.unpickle(filepath)
+        test_data = test_dic['data']
+        test_labels = test_dic['labels']
+        return test_data, test_labels
+
+    def extract_label_names(self):
+        label_dic = self.unpickle('./data/cifar-100-batches-py/batches.meta')
+        label_names = label_dic['label_names']
+        return label_names
---
+++ cnn_active_learning/dl-CIFAR100.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+DATA_DIR="./data"
+UNZIP_NAME="cifar-100-batches-py"
+ARCHIVE_NAME="cifar100.tar.gz"
+DATASET_URL=https://www.cs.toronto.edu/\~kriz/cifar-100-python.tar.gz
+
+if [ ! -d "${DATA_DIR}/${UNZIP_NAME}" ]; then
+    if [ ! -f "${DATA_DIR}/${ARCHIVE_NAME}" ]; then
+        echo "Downloading CIFAR100 dataset..."
+        curl ${DATASET_URL} > "${DATA_DIR}/${ARCHIVE_NAME}"
+    fi
+
+    if [ -f "${DATA_DIR}/${ARCHIVE_NAME}" ]; then
+        echo "Unzip..."
+        tar -xzvf ${DATA_DIR}/${ARCHIVE_NAME} -C ${DATA_DIR}
+        rm ${DATA_DIR}/${ARCHIVE_NAME}
+    else echo "The archive is not present" && exit 1
+    fi
+else
+    echo "CIFAR100 already downloaded"
+fi
+
+
