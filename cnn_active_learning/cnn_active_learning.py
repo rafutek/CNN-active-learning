114bdbd042acd68433a9cbd08629b058f276fed6
linur <raphaelroy@tutanota.com>
2020-03-20 22:38:15 -0400

    active learning loop

--- cnn_active_learning/cnn_active_learning.py
+++ cnn_active_learning/cnn_active_learning.py
@@ -14,25 +14,23 @@ num_learnings = 2
 k=50
 idx_labeled_samples = np.arange(k)
 
-print(range(num_learning))
-return
-dataManager = DataManager(data=data, \
+for num_learning in range(num_learnings):
+    print('\nIndexes of samples for training:\n',idx_labeled_samples)
+    dataManager = DataManager(data=data, \
         idx_labeled_samples=idx_labeled_samples, \
-        batch_size=1)
+        batch_size=10)
 
-optimizer = optimizer_setup(torch.optim.SGD, lr=0.001, momentum=0.9)
-vgg = VggNet(num_classes=10)
+    optimizer = optimizer_setup(torch.optim.SGD, lr=0.001, momentum=0.9)
+    vgg = VggNet(num_classes=10)
 
-netTrainer = NetTrainer(model=vgg, \
-        data_manager=dataManager, \
-        loss_fn=nn.CrossEntropyLoss() , \
-        optimizer_factory=optimizer)
+    netTrainer = NetTrainer(model=vgg, \
+            data_manager=dataManager, \
+           loss_fn=nn.CrossEntropyLoss() , \
+           optimizer_factory=optimizer)
 
-netTrainer.train(2)
-selected_samples_idx = netTrainer.evaluate_on_validation_set(k)
-add_to_train_idx = selected_samples_idx[1,:].astype(int)
-
-idx_labeled_samples = np.concatenate((idx_labeled_samples, add_to_train_idx))
-print(len(idx_labeled_samples))
-print(idx_labeled_samples)
+    netTrainer.train(2)
+    selected_samples_idx = netTrainer.evaluate_on_validation_set(k)
+    add_to_train_idx = selected_samples_idx[1,:].astype(int)
+    print('k indexes of lower confidence classification:\n',add_to_train_idx)
+    idx_labeled_samples = np.concatenate((idx_labeled_samples, add_to_train_idx))
 
