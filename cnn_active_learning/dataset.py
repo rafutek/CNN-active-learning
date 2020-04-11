f74c5df380de0027005d5205e50f18115f8844a1
linur <raphaelroy@tutanota.com>
2020-04-11 11:11:10 -0400

    add comments

--- cnn_active_learning/dataset.py
+++ cnn_active_learning/dataset.py
@@ -3,7 +3,16 @@ import torchvision.transforms as transforms
 import torch
 
 class Dataset(Dataset):
+    """
+    Class to create a torch Dataset from local data
+    """
     def __init__(self, samples, labels):
+        """
+        Constructor that set dataset variables
+        Args:
+            samples: array containing the dataset samples
+            labels: array containing the label of each sample
+        """
         if len(samples) ==  len(labels):
             self.samples = samples
             self.labels = labels
@@ -14,9 +23,22 @@ class Dataset(Dataset):
         ])
 
     def __len__(self):
+        """
+        Function to get the length of the dataset
+        Returns:
+            The length of the dataset
+        """
         return len(self.samples)
 
     def __getitem__(self, index):
+        """
+        Function to get an item of the dataset
+        Args:
+            index: the position of the dataset item
+        Returns:
+            sample: the associated sample as a normalized tensor
+            label: the associated label
+        """
         sample, label = self.samples[index], self.labels[index]
         sample = torch.tensor(sample, dtype=torch.float)
         sample = self.transform(sample)
